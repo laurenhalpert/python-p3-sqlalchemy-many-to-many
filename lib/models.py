02806ebf4dcb609c0b8b974be2cbc33be37a3b22
Lauren <halpertln@gmail.com>
2023-05-10 15:28:39 -0600

    complete lab updated

--- lib/models.py
+++ lib/models.py
@@ -3,12 +3,20 @@ from sqlalchemy import ForeignKey, Table, Column, Integer, String, DateTime, Met
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
-
+# convention = {
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+# }
+# metadata = MetaData(naming_convention=convention)
+engine = create_engine("sqlite:///many_to_many.db")
 Base = declarative_base(metadata=metadata)
+
+game_user = Table(
+    'game_users',
+    Base.metadata,
+    Column('game_id', ForeignKey('games.id'), primary_key=True),
+    Column('user_id', ForeignKey('users.id'), primary_key=True),
+    extend_existing=True,
+)
 
 class Game(Base):
     __tablename__ = 'games'
@@ -19,12 +27,29 @@ class Game(Base):
     platform = Column(String())
     price = Column(Integer())
 
-    reviews = relationship('Review', backref=backref('game'))
+    users = relationship('User', secondary=game_user, back_populates='games')
+    reviews = relationship('Review', backref=backref('game'), cascade='all, delete-orphan')
+    
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
             f'title={self.title}, ' + \
             f'platform={self.platform})'
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+
+    games = relationship('Game', secondary=game_user, back_populates='users')
+    reviews = relationship('Review', backref=backref('user'), cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'User(id={self.id}, ' + \
+            f'name={self.name})'
 
 class Review(Base):
     __tablename__ = 'reviews'
@@ -42,17 +67,5 @@ class Review(Base):
             f'score={self.score}, ' + \
             f'game_id={self.game_id})'
 
-class User(Base):
-    __tablename__ = 'users'
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    created_at = Column(DateTime(), server_default=func.now())
-    updated_at = Column(DateTime(), onupdate=func.now())
-
-    reviews = relationship("Review", backref=backref("user"))
-
-    def __repr__(self):
-        return f'User(id={self.id}, ' + \
-            f'name={self.name})'
     
